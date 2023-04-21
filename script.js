f97abca5a914222354e9c0fc53f35cab13c21bca
MaxPower <89873437+MaxPowah@users.noreply.github.com>
2023-04-21 12:11:55 +0200

    it works

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 var c = document.getElementById("gameScreen");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "black";
-ctx.lineWidth = 5;
+ctx.lineWidth = 4;
 ctx.font = "20px Roboto";
+let mousePos = {x: 0, y: 0};
+//report the mouse position on click
+c.addEventListener("click", function (evt) {
+  getMousePos(c, evt);
+}, false);
+//Get Mouse Position
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
 
+  
+  mousePos.x = evt.clientX - rect.left;
+  mousePos.y = evt.clientY - rect.top;
+}
+
+/*
 let mX;//mouse x pos
 let mY;//mouse y pos
-/*
+
+
 function  getMousePos(canvas, evt) { //?fattar inte
   var rect = canvas.getBoundingClientRect(), // abs. size of element
   scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
@@ -18,60 +33,75 @@ function  getMousePos(canvas, evt) { //?fattar inte
   }
 }
 */
-function GetMousePos()
-{
-  //code
-  return mX, mY;
-}
+let skitenJagValtNu;
 function Draw(something)
 {
-  //mX = GetMousePos.mX;
-  //mY = GetMousePos.mY;
-  mX = Math.floor(Math.random() * 700);
-  mY = Math.floor(Math.random() * 480);
+  skitenJagValtNu = something;
+  /*
+  button is pressed
+  canvas is clicked
+  stuff is drawn
+  */
+  //mX = Math.floor(Math.random() * 960);
+  //mY = Math.floor(Math.random() * 640);
 
-  GetMousePos();
+
   switch(something)
   {
     case "battery":
-      DrawBattery(mX, mY);
+      DrawBattery(mousePos.x, mousePos.y);
       break;
     case "resistor":
-      DrawResistor(mX, mY);
-      break;
+        DrawResistor(mousePos.x, mousePos.y);
+        break;
     case "conductor":
-      let mX2 = Math.floor(Math.random() * 700);
-      let mY2 = Math.floor(Math.random() * 480);
-      DrawConductor(mX, mY, mX2, mY2);
+      DrawConductor();
       break;
     case "lamp":
       break;
-    case "volt":
+      case "volt":
       break;
     case "clearAll":
       break;
     default:
       alert("hmm, något gick fel");
       break;
-  }
+    }
 }
 
 function DrawBattery(mX, mY) //!can add 3rd arg to change the voltage
 {
-  ctx.fillStyle = "black";
+  let U = document.getElementById("voltage").value;
+  let voltage = U.toString();
+  if(voltage == "" || voltage == "0")
+  {
+    voltage = "12";
+    U = 12; // för beräkningar
+  }
   
+  let Uconcat = voltage.concat(" V");
+  ctx.fillStyle = "black";
   ctx.fillRect(mX - 10, mY, 6, 30);
   ctx.fillRect((mX + 10), (mY - 14), 4, 60);
   //draw text elements
   let connectMinus = mX - 20; // platsen vid minuspolen där andra saker kan anslutas
   let connectPlus = mX + 30; // samma för pluspolen
-  DrawBattery(mX, mY);
+  ctx.fillText(Uconcat, mX-20, mY - 20);
+
   return connectPlus, connectMinus;
   
 }
     
 function DrawResistor(mX, mY)
 {
+  let R = document.getElementById("resistor").value;
+  let resistance = R.toString();
+  if(resistance == "" || resistance == "0")
+  {
+    resistance = "10";
+    R = 10; // för beräkningar
+  }
+  let Rconcat = resistance.concat(" Ω");
   ctx.fillStyle = "black";
   
   //jag skulle vilja att den kan roteras
@@ -83,7 +113,7 @@ function DrawResistor(mX, mY)
   let connectPlus = mX + 30; // samma för pluspolen
   
   ctx.moveTo(mX-40,mY-20);
-  ctx.fillText("x ohm", mX-35, mY+20);
+  ctx.fillText(Rconcat, mX-35, mY+20);
   /*
   ctx.fillStyle = "rgb(234, 242, 248)"; // ser till att insidan är samma färg som bakgrunden
   ctx.fillRect((mX- width/2) + insideOffset/2, (mY + insideOffset/2), (width - insideOffset), height - insideOffset);
